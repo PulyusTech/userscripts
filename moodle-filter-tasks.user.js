c384afeab0b9d4bb85da91550506a421152da049
PulyusTech <pulyustech@gmail.com>
2024-03-06 11:26:36 +1100

    autoupdate

--- moodle-filter-tasks.user.js
+++ moodle-filter-tasks.user.js
@@ -2,7 +2,7 @@
 // @name        UNSW Moodle Filter Tasks
 // @namespace   https://pulyustech.github.io/userscripts
 // @match       https://moodle.telt.unsw.edu.au/*
-// @version     1.0
+// @version     1.0.1
 // @author      PulyusTech
 // @description Filters the sidebar to only show tasks.
 // @icon        https://my.unsw.edu.au/images-channel/SADP/moodle.png
@@ -19,13 +19,17 @@
     el.onclick = () => {
       if (el.innerHTML == "Filter: ON") {
         el.innerHTML = "Filter: OFF";
-        for (let item of document.querySelectorAll("li")) {
+        for (let item of document
+          .getElementById("theme_remui-drawers-courseindex")
+          .querySelectorAll("li")) {
           if (item.querySelectorAll("i").length == 2)
             item.setAttribute("style", "display:flex !important");
         }
       } else {
         el.innerHTML = "Filter: ON";
-        for (let item of document.querySelectorAll("li")) {
+        for (let item of document
+          .getElementById("theme_remui-drawers-courseindex")
+          .querySelectorAll("li")) {
           if (item.querySelectorAll("i").length == 2)
             item.setAttribute("style", "display:none !important");
         }
