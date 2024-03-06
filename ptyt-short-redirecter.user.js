9c7d5a22dd8ee14541c474cdd3d631dd40ccf6e2
PulyusTech <pulyustech@gmail.com>
2024-03-06 18:04:08 +1100

    autoupdate

--- ptyt-short-redirecter.user.js
+++ ptyt-short-redirecter.user.js
@@ -3,15 +3,22 @@
 // @namespace    https://pulyustech.github.io/userscripts
 // @match        https://*.youtube.com/*
 // @grant        none
-// @version      1.0
+// @version      1.0.1
 // @author       PulyusTech
 // @description  Makes all youtube shorts play as a normal video.
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=YouTube.com
 // ==/UserScript==
-setInterval(() => {
-  if (window.location.href.includes("/shorts/"))
-    window.location.href = window.location.href.replace(
-      "/shorts/",
-      "/watch?v="
-    );
-}, 100);
+let u = "";
+let l = () => {
+  if (window.location.href != u) {
+    if (window.location.href.includes("/shorts/")) {
+      window.location.href = window.location.href.replace(
+        "/shorts/",
+        "/watch?v="
+      );
+    }
+    u = window.location.href;
+  }
+  setTimeout(l, 100);
+};
+l();
