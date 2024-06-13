3c20c37012daadb3fca38bcb6155ad5d4f77515b
PulyusTech <pulyustech@gmail.com>
2024-06-13 15:08:59 +1000

    autoupdate

--- AISoullink.user.js
+++ AISoullink.user.js
@@ -9,6 +9,14 @@
 // @author      PulyusTech
 // @description Remotely send requests to AI from another tab
 // ==/UserScript==
+
+//How to use:
+//You can now highlight text in Moodle you aren't supposed to, and highlights are now invisible
+//Open ChatGPT and type anything in the prompt to initiate
+//Press Shift to record highlighed text in Moodle
+//Press Enter to ask ChatGPT the recorded text from Moodle
+//After ChatGPT responds, the Moodle quiz question multiple choices will be replaced with the answers
+
 document.getElementByAttVal = function (attribute, value) {
   for (let el of document.getElementsByTagName("*")) {
     if (el.hasAttribute(attribute)) {
@@ -19,6 +27,8 @@ document.getElementByAttVal = function (attribute, value) {
 };
 (async () => {
   if (window.location.href.includes("moodle")) {
+    let answerNow = await GM_getValue("answerNow");
+    if (answerNow == "answering") await GM_setValue("answerNow", "answerDone");
     document.head.insertAdjacentHTML(
       "beforeend",
       `<style>
@@ -30,38 +40,37 @@ document.getElementByAttVal = function (attribute, value) {
     );
     document.body.contentEditable = true;
     document.body.spellcheck = false;
-    document.body.oncopy = async () => {
-      let t = window.getSelection().toString();
-      if (t) {
-        let prevQ = await GM_getValue("q");
-        await GM_setValue("q", prevQ + "\n\n" + t);
-        document.body.contentEditable = false;
+    document.onkeydown = async (event) => {
+      switch (event.key) {
+        case "Shift":
+          let t = window.getSelection().toString();
+          if (t) {
+            let prevQ = await GM_getValue("q");
+            await GM_setValue("q", prevQ + "\n\n" + t);
+            document.body.contentEditable = false;
+          }
+          break;
+        case "Enter":
+          await GM_setValue("answerNow", "answering");
+          l();
+          break;
       }
     };
-    document.onkeydown = async (event) => {
-      if (event.key == "Enter") await GM_setValue("answerNow", "answering");
-    };
     let l = async () => {
-      let answerNow = await GM_getValue("answerNow");
-      if (answerNow == "answerDone") {
-        let a = await GM_getValue("a");
-        console.log(a);
-        let options = "";
-        for (let el of a.split("\n")) {
-          if (el != "") {
-            options += `
+      let a = await GM_getValue("a");
+      let options = "";
+      for (let el of a.split("\n")) {
+        if (el != "") {
+          options += `
             <div class="option">
               <input type="radio" /><label>${el}</label>
             </div>
             `;
-          }
         }
-        document.querySelector(".answer").innerHTML = options;
-      } else {
-        setTimeout(l, 1000);
       }
+      document.querySelector(".answer").innerHTML = options;
+      setTimeout(l, 1000); //interval to update moodle fake mc answers on creen
     };
-    l();
   } else if (window.location.href.includes("chatgpt")) {
     await GM_setValue("q", "");
     await GM_setValue("a", "");
@@ -74,21 +83,16 @@ document.getElementByAttVal = function (attribute, value) {
         document
           .getElementByAttVal("data-testid", "fruitjuice-send-button")
           .click();
-        let prevA = "AMOGUS";
         let l2 = async () => {
           let res = document.getElementsByClassName("markdown");
           let a = res[res.length - 1].innerHTML.replace(/<p>|<\/p>/g, "\n");
-          if (a != prevA) {
-            prevA = a;
-            setTimeout(l2, 1000);
-          } else {
-            await GM_setValue("a", a);
-            await GM_setValue("answerNow", "answerDone");
-          }
+          await GM_setValue("a", a);
+          let answerNow2 = await GM_getValue("answerNow");
+          if (answerNow2 == "answering") setTimeout(l2, 500); //interval to update moodle fake mc answers
         };
-        setTimeout(l2, 5000);
+        setTimeout(l2, 1000); //delay before checking response
       } else {
-        setTimeout(l, 1000);
+        setTimeout(l, 1000); //interval to check if question is submitted
       }
     };
     l();
